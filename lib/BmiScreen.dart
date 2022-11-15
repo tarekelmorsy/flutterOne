a93cec24933397ab46beca34916dccd8bfd3d2f0
tarekmohamed <tarek@baianat.com>
2022-11-15 18:04:24 +0200

    Bmi 1

--- lib/BmiScreen.dart
+++ lib/BmiScreen.dart
@@ -9,6 +9,11 @@ class BmiScreen extends StatefulWidget {
 }
 
 class _BmiScreenState extends State<BmiScreen> {
+  bool isMAll=false ;
+  double height =100;
+  int age=20;
+  int weight=50;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,28 +27,33 @@ class _BmiScreenState extends State<BmiScreen> {
               padding: const EdgeInsets.all(20.0),
               child: Row(children: [
                 Expanded(
-                  child: Container(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.access_alarm,
-                          size: 80,
-                        ),
-                        SizedBox(
-                          width: 10,
-                          height: 10,
-                        ),
-                        Text(
-                          "MALL",
-                          style: TextStyle(fontSize: 30),
-                        )
-                      ],
+                  child: GestureDetector(
+                    onTap: (){
+                    setState(() {
+                      isMAll=true;
+                    });
+                    },
+                    child: Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Image(image: AssetImage('images/ic_following.png'),width: 90,height: 90,)
+                           ,
+                          SizedBox(
+                            width: 10,
+                            height: 10,
+                          ),
+                          Text(
+                            "MALL",
+                            style: TextStyle(fontSize: 30),
+                          )
+                        ],
+                      ),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: !isMAll?Colors.grey[350]:Colors.blue),
                     ),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.grey[350]),
                   ),
                 ),
                 SizedBox(
@@ -51,28 +61,32 @@ class _BmiScreenState extends State<BmiScreen> {
                   height: 20,
                 ),
                 Expanded(
-                  child: Container(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.access_alarm,
-                          size: 80,
-                        ),
-                        SizedBox(
-                          width: 10,
-                          height: 10,
-                        ),
-                        Text(
-                          "MALL",
-                          style: TextStyle(fontSize: 30),
-                        )
-                      ],
+                  child:  GestureDetector(
+                    onTap: (){
+                      setState(() {
+                        isMAll=false;
+                      });
+                    },
+                    child: Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Image(image: AssetImage('images/ic_shares.png'),width: 90,height: 90,),
+                          SizedBox(
+                            width: 10,
+                            height: 10,
+                          ),
+                          Text(
+                            "FEMALL",
+                            style: TextStyle(fontSize: 30),
+                          )
+                        ],
+                      ),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color:  isMAll?Colors.grey[350]:Colors.blue),
                     ),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.grey),
                   ),
                 ),
               ]),
@@ -80,47 +94,154 @@ class _BmiScreenState extends State<BmiScreen> {
           ),
           Expanded(
               child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20 ),
-                child: Container(
-          decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: Colors.grey[350]),
-
-                  child: Column(
-
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.grey[350]),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "HGIGHT",
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
                     mainAxisAlignment: MainAxisAlignment.center,
+                    textBaseline: TextBaseline.alphabetic,
                     children: [
                       Text(
-                        "HGIGHT",
-                        style: TextStyle(fontSize: 30),
+                        "${height.round()}",
+                        style: TextStyle(
+                            fontSize: 50, fontWeight: FontWeight.w900),
                       ),
-                      SizedBox(height: 10 ,),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.baseline,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        textBaseline: TextBaseline.alphabetic,
-                        children: [ Text(
-                        "190",
-                        style: TextStyle(fontSize: 50  ,fontWeight: FontWeight.w900),
-                      ), Text(
+                      Text(
                         "CM",
                         style: TextStyle(fontSize: 15),
-                      )],),
-                      Slider(value: 100
-                          ,max: 300,min: 50, onChanged: (value){})
-
+                      )
                     ],
+                  ),
+                  Slider(value: height, max: 300, min: 50, onChanged: (value) {
 
-          ),
-                ),
-              )),
-          Expanded(
-              child: Container(
-            color: Colors.teal,
+                    setState(() {
+                      height=value;
+
+                    });
+                  })
+                ],
+              ),
+            ),
           )),
-          MaterialButton(
-            onPressed: () {},
-            child: Text("CALCULAT"),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.grey[350]),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "AGE",
+                            style: TextStyle(fontSize: 30),
+                          ),
+                          Text(
+                            "${age.round()}",
+                            style: TextStyle(
+                                fontSize: 50, fontWeight: FontWeight.w900),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              FloatingActionButton(
+                                onPressed: () {
+                                  setState(() {
+                                    age--;
+                                  });
+                                },
+                                child: Icon(Icons.remove),
+                                mini: true,
+                              ),
+                              FloatingActionButton(
+                                onPressed: () { setState(() {
+                                  age++;
+                                });},
+                                child: Icon(Icons.add),
+                                mini: true,
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.grey[350]),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "WEIGHT",
+                            style: TextStyle(fontSize: 30),
+                          ),
+                          Text(
+                            "${weight.round()}",
+                            style: TextStyle(
+                                fontSize: 50, fontWeight: FontWeight.w900),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              FloatingActionButton(
+                                onPressed: () {
+                                  setState(() {
+                                    weight--;
+                                  });
+                                },
+                                child: Icon(Icons.remove),
+                                mini: true,
+                              ),
+                              FloatingActionButton(
+                                onPressed: () { setState(() {
+                                  weight++;
+                                });},
+                                child: Icon(Icons.add),
+                                mini: true,
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )),
+          Container(
+            width: double.infinity,
+            child: MaterialButton(
+
+              onPressed: () {},
+              child: Text("CALCULAT"),
+              color: Colors.blue,
+            ),
           )
         ],
       ),
